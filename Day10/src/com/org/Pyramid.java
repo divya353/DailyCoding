a9aec51a7f41b15efc83e0e269ef4a24388bfc41
divya353 <divyapatil0508@gmail.com>
2025-11-27 20:11:46 +0530

    Add files via upload

---
+++ Day10/src/com/org/Pyramid.java
@@ -0,0 +1,25 @@
+package com.org;
+
+import java.util.Scanner;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter size:");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+		    for(int j=n;j>i;j--) {
+		        System.out.print(" ");
+		    }
+		    for(int k=1;k<=i;k++) {
+		        System.out.print(" *");
+		    }
+		    System.out.println();
+		}
+
+
+	}
+
+}
